98a5b45b485beba88b8354a9d9835c0b16857c7a
jichen.liu85 <jichen.liu85@gmail.com>
2023-01-30 18:47:29 -0500

    5 nd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -26,8 +26,8 @@ class Controller extends BaseController
         $body = $request->input();
         $body = json_encode($body);
         $log = "body is ".$body."   header is ".$header;
-        $path = base_path('\tmp\api-logs.log');
-        error_log($log);
+        $path = base_path('tmp/api-logs.log');
+        error_log($log,3,$path);
         $file_path1 = base_path ('\tmp\api-logs.log');
         $file_path2 = 'C:\TMS-Lumen\msi_dev\storage\logs\my-errors.log';
        // error_log($log,3,'/storage/logs/api-logs.log');
